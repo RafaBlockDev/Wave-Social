57efde65223ab0a8fdaa4689bd6edb3ee29ea978
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-05-20 13:31:44 -0500

    Hardhat correct confirm

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.5",
   networks: {
-    rinkeby: {
+    mumbai: {
       url: process.env.ALCHEMY_MUMBAI_URL,
-      accounts: process.env.PRIVATE_KEY_MUMBAI
+      account: process.env.PRIVATE_KEY_MUMBAI
     },
   },
 };
