99491867fb82b67e6066b2ec2a5b5c6e444ef79b
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-05-20 12:35:20 -0500

    Adding elements to solidity and deploy

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("Que onda wey!, Soy un smart contract... jeje");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return (totalWaves);
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -4,10 +4,21 @@ const { hexStripZeros } = require("@ethersproject/bytes")
 // Hardhat Runtime Environment = hre
 
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+
     console.log("🚀 Contract deployed to: ", waveContract.address);
+    console.log("👻 Contract deployed by: ", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
