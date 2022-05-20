093c3d0478d309ed5b2b203c2c4511a2e2ce3bc3
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-05-20 12:03:33 -0500

    Script to deploy at localhost the smart contrat

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Que onda wey!, Soy un smart contract... jeje");
+    }
+}
