44f0f2ac272993b6cd37543cce619ad600c1a4c6
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-05-24 23:42:52 -0500

    Adding wave-social media

--- contract/README.md
+++ contract/README.md
@@ -1,6 +1,4 @@
-# Wave-Social
 
-Wave Social is a social media where you can wave to anyone and present yourself paying a fee (obviusly) and get the probably to win MATIC 🥳
 
 # Basic Sample Hardhat Project
 
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function App() {
+
   return (
     <div className={styles.mainContainer}>
       <div className={styles.dataContainer}>
         <div className={styles.header}>
-        👋 Hey there!
+        👋 Hi there!
         </div>
 
-      <div className={styles.bio}>
+        <div className={styles.bio}>
         <ConnectButton className={styles.waveButton}/>
       </div>
       </div>
